5ed662de509ba94e61368b09ab8e7fa0d07199ae
lichruslan <lichruslan@github.com>
2019-03-03 14:27:16 +0600

    Был дабавлены утилиты CORS and MORDAN для удобного логирования данных и большего понимания

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+// const cors = require('cors'); //Нужен для обработки корс запросы
 const authRoutes = require('./routes/auth');
 const analyticsRoutes = require('./routes/analytics');
 const categoryRoutes = require('./routes/category');
@@ -7,8 +8,12 @@ const orderRoutes = require('./routes/order');
 const positionRoutes = require('./routes/position');
 const app = express();
 
+
+app.use(require('morgan')('dev'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+app.use(require('cors')())
 
 app.use('/api/auth', authRoutes);
 app.use('/api/analytics', analyticsRoutes);
