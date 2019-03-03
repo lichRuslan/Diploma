1e14647286199dc54c2ed562c5275c4dba57a627
lichruslan <lichruslan@github.com>
2019-03-03 22:18:57 +0600

    Была реализована полноценная регистрацыя на стороне сервера, с методом шифрования bcrypt

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 
 
@@ -23,6 +24,21 @@ module.exports.register = async function (req, res){
         });
     }else {
         //создать пользователя
+        const salt = bcrypt.genSaltSync(10);
+        const password = req.body.password;
+        const user = new User({
+            email: req.body.email,
+            password: bcrypt.hashSync(password, salt)
+            // password: req.body.password
+        })
+        try {
+            await user.save()
+            res.status(201).json(user)
+        } catch (e){
+            // error Обработать ошибку !
+
+
+        }
 
     }
 }
