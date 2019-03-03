e9574b668e2dde9eb3c866e8efbc968b4156e029
lichruslan <lichruslan@github.com>
2019-03-03 18:09:31 +0600

    Была отправка данных с регистрации, можно начинать работать с базай

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,6 @@
+const User = require('../models/User');
+
+
 module.exports.login = function (req, res){
     res.status(200).json({
         login: {
@@ -10,7 +13,10 @@ module.exports.login = function (req, res){
 
 
 module.exports.register = function (req, res){
-    res.status(200).json({
-        register: 'from controller'
+    //mail password
+    const user = new User({
+        email: req.body.email,
+        password: req.body.password
     });
+    user.save().then(()=> console.log('User created!'))
 }
