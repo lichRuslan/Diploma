51b87b58b3d46536b8ffa0f491366909833efe51
lichruslan <lichruslan@github.com>
2019-03-09 19:45:49 +0600

    Было реализован функционал плзиций

--- controllers/position.js
+++ controllers/position.js
@@ -5,7 +5,7 @@ const errorHendler =require('../utils/errorHendler');
 module.exports.getByCategoryId = async function (req,res){
     try {
         const positions = await Position.find({
-            categoty: req.parems.categoryId,
+            category: req.params.categoryId,
             user: req.user.id
         });
         res.status(200).json(positions)
@@ -13,23 +13,37 @@ module.exports.getByCategoryId = async function (req,res){
         errorHendler(res, e);
     }
 }
-module.exports.create = function (req,res){
+module.exports.create = async function (req,res){
     try {
-
+        const position = await new Position({
+            name: req.body.name,
+            cost: req.body.cost,
+            category: req.body.category,
+            user: req.usage.id
+        }).save()
+        res.status(201).json(position)
     }catch (e) {
         errorHendler(res, e);
     }
 }
-module.exports.remove = function (req,res){
+module.exports.remove = async function (req,res){
     try {
-
+        await Position.remove({_id: req.params.id})
+        res.status(200).json({
+            message: 'Позицыя была удалена.'
+        })
     }catch (e) {
         errorHendler(res, e);
     }
 }
-module.exports.update = function (req,res){
+module.exports.update = async function (req,res){
     try {
-
+        const position = await Position.findOneAndUpdate(
+            {_id: req.body.params.id},
+            {$set: req.body},
+            {new: true}
+            )
+        res.status(200).json(position)
     }catch (e) {
         errorHendler(res, e);
     }
