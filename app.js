e72fecd8c3052cd1b0139d7e4ce3b100ff160e44
lichruslan <lichruslan@github.com>
2019-03-09 21:54:12 +0600

    Было реализован создание категорий

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ require('./middleware/passport')(passport)
 
 
 app.use(require('morgan')('dev'));
+app.use('/uploads', express.static('uploads'))
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
