5fc253b2641c4fa2e9fa0a8c55efd4967a6fb278
lichruslan <lichruslan@github.com>
2019-06-21 04:51:51 +0600

    Добавление страниц

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -12,6 +12,10 @@ import { RegisterPageComponent } from './register-page/register-page.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { TokenInterceptor } from './shared/classes/token.interceptor';
 import { OverviewPageComponent } from './overview-page/overview-page.component';
+import { AnalyticsPageComponent } from './analytics-page/analytics-page.component';
+import { HistoryPageComponent } from './history-page/history-page.component';
+import { OrderPageComponent } from './order-page/order-page.component';
+import { CategoriesPageComponent } from './categories-page/categories-page.component';
 // import { from } from 'rxjs';
 
 @NgModule({
@@ -21,7 +25,11 @@ import { OverviewPageComponent } from './overview-page/overview-page.component';
     AuthLayoutComponent,
     SiteLayoutComponent,
     RegisterPageComponent,
-    OverviewPageComponent
+    OverviewPageComponent,
+    AnalyticsPageComponent,
+    HistoryPageComponent,
+    OrderPageComponent,
+    CategoriesPageComponent
   ],
   imports: [
     BrowserModule,
