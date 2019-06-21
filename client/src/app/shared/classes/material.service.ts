9717ebdacccaf80b05dd02bcf344d84435b33023
lichruslan <lichruslan@github.com>
2019-06-21 08:30:02 +0600

    Доделал Добавление и редактирование категорий.

--- client/src/app/shared/classes/material.service.ts
+++ client/src/app/shared/classes/material.service.ts
@@ -9,4 +9,7 @@ export class MaterialService{
     static initializeFloatingButton(ref: ElementRef){
         M.FloatingActionButton.init(ref.nativeElement)
     }
+    static updateTextInputs(){
+        M.updateTextFields()
+    }
 }
